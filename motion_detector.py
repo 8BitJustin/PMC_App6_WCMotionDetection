7c61d9a76a8984d66e05e1b0c2b89b7263aa7a4e
Justin Olson <j.olson.digital@gmail.com>
2019-08-27 18:17:36 -0700

    Changed iteration back to 2, increased contour to 10000.

--- motion_detector.py
+++ motion_detector.py
@@ -5,7 +5,7 @@ video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -15,14 +15,14 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray)
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
-    # dropped iterations to 0 as outline wasn't thick enough at 2
-    thresh_frame = cv2.dilate(thresh_frame, None, iterations=0)
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     (conts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in conts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status = 1
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
@@ -32,11 +32,11 @@ while True:
     cv2.imshow("Color Frame", frame)
 
     key = cv2.waitKey(1)
-    print(gray)
-    print(delta_frame)
 
     if key == ord('q'):
         break
+
+    print(status)
 
 video.release()
 cv2.destroyAllWindows()
